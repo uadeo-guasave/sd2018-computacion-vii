9b14ac48cb0b25a1f7cbacc41fd0d7a9e2c6505c
bidkar <bidkar.aragon@udo.mx>
2018-10-15 10:23:31 -0600

    Contexto Sqlite
    Tabla Usuarios

--- QueOnda/Entidades/SQLiteDbContext.cs
+++ QueOnda/Entidades/SQLiteDbContext.cs
@@ -7,8 +7,37 @@ namespace QueOnda.Entidades
 {
     class SQLiteDbContext : DbContext
     {
-        public DbSet<Usuario> Usuarios { get; set; }
+        // TODO: definir la conexion a utilizar
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source=db/queonda.db");
+            base.OnConfiguring(optionsBuilder);
+        }
 
-        
+        // TODO: definir la estructura de las tablas
+        // en caso de no existir una (Fluent API)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(usuario =>
+            {
+                usuario.HasIndex(i => i.NombreDeUsuario).IsUnique();
+                usuario.HasIndex(i => i.CorreoElectronico).IsUnique();
+                usuario.HasData(
+                    new Usuario
+                    {
+                        NombreDeUsuario = "bidkar",
+                        Contraseña = "123",
+                        Nombre = "Bidkar",
+                        Apellidos = "Aragon",
+                        CorreoElectronico = "bidkar.aragon@udo.mx",
+                        Id = 1
+                    });
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
--- QueOnda/Entidades/Usuario.cs
+++ QueOnda/Entidades/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Mail;
 using System.Text;
 
@@ -8,11 +9,23 @@ namespace QueOnda.Entidades
     class Usuario
     {
         public int Id { get; set; }
+
+        [Required, MaxLength(12)]
         public string NombreDeUsuario { get; set; }
+
+        [Required, MaxLength(16)]
         public string Contraseña { get; set; }
+
+        [Required, MaxLength(50)]
         public string Nombre { get; set; }
+
+        [Required, MaxLength(50)]
         public string Apellidos { get; set; }
-        public MailAddress CorreoElectronico { get; set; }
+
+        [Required, MaxLength(200)]
+        public string CorreoElectronico { get; set; }
+
+        [MaxLength(30)]
         public string Telefono { get; set; }
     }
 }
